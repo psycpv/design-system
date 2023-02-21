1135439465c1a0745da60241a20887c7c5dcb136
David Torres <jdtcto@gmail.com>
2023-02-21 16:45:49 -0500

    NWEB-251: add banner helpers variation with alert state

--- src/atoms/DzBanner.tsx
+++ src/atoms/DzBanner.tsx
@@ -10,6 +10,24 @@ const VARIANTS = {
   ERROR: 'error',
 };
 
+const ALERT_VARIANT = {
+  DEFAULT: 'mDefault',
+  DARK: 'mDark',
+  SUCCESS: 'mSuccess',
+  ERROR: 'mError',
+};
+
+const TYPES = {
+  MESSAGE: 'messageType',
+  ALERT: 'alertType',
+};
+export const ALERT_VARIANT_NAMES = [
+  ALERT_VARIANT.DEFAULT,
+  ALERT_VARIANT.DARK,
+  ALERT_VARIANT.SUCCESS,
+  ALERT_VARIANT.ERROR,
+] as const;
+export const BANNER_TYPES_NAMES = [TYPES.MESSAGE, TYPES.ALERT] as const;
 export const BANNER_VARIANT_NAMES = [
   VARIANTS.DEFAULT,
   VARIANTS.SUCCESS,
@@ -17,21 +35,45 @@ export const BANNER_VARIANT_NAMES = [
   VARIANTS.WARNING,
   VARIANTS.ERROR,
 ] as const;
+export type BannerType = typeof BANNER_TYPES_NAMES[number];
 export type BannerVariant = typeof BANNER_VARIANT_NAMES[number];
+export type BannerAlertVariant = typeof ALERT_VARIANT_NAMES[number];
+
 export interface DzBannerProps {
   title: string;
   subtitle?: string;
   variant?: BannerVariant;
+  type?: BannerType;
+  alertType?: BannerAlertVariant;
 }
 
 const styles: any = {
+  mDefault: `
+    bg-black-10
+  `,
+  mDark: `
+    bg-black-100
+  `,
+  mSuccess: `
+    bg-green-100
+  `,
+  mError: `
+    bg-red-100
+  `,
   bannerContainer: `
     flex
     p-5
   `,
   textContainer: `
     flex
+  `,
+  messageType: `
     flex-col
+  `,
+  alertType: `
+    flex-row
+    justify-center
+    py-2.5
   `,
   title: {
     default: `
@@ -43,12 +85,24 @@ const styles: any = {
     alert: `
       text-red-40
     `,
-    warning:`
+    warning: `
       text-yellow-40
     `,
-    error:`
+    error: `
       text-red-100
-    `
+    `,
+    mDefault: `
+      text-black-100
+    `,
+    mDark: `
+      text-white-100
+    `,
+    mSuccess: `
+      text-white-100
+    `,
+    mError: `
+      text-white-100
+    `,
   },
   default: `
     bg-black-10
@@ -59,21 +113,27 @@ const styles: any = {
   alert: `
     bg-red-10
   `,
-  warning:`
+  warning: `
     bg-yellow-10
   `,
-  error:`
+  error: `
     bg-red-60
-  `
+  `,
 };
 export const DzBanner: FC<DzBannerProps> = ({
   title = '',
   subtitle = '',
   variant = VARIANTS.DEFAULT,
+  type = TYPES.MESSAGE,
+  alertType = ALERT_VARIANT.DEFAULT,
 }) => {
+  const alertTypeStyles = type === TYPES.ALERT ? styles?.[alertType] : '';
+  const alertTitleStyles =
+    type === TYPES.ALERT ? styles?.title?.[alertType] : '';
+
   const titleSection = title ? (
     <DzText
-      className={cn(styles.title?.[variant])}
+      className={cn(styles.title?.[variant], alertTitleStyles)}
       textSize={TEXT_SIZES.XS}
       textType={TEXT_TYPES.LABEL}
       text={title}
@@ -81,16 +141,24 @@ export const DzBanner: FC<DzBannerProps> = ({
     />
   ) : null;
 
-  const subTitle = subtitle ? (
-    <DzText
-      textSize={TEXT_SIZES.XS}
-      textType={TEXT_TYPES.P}
-      text={subtitle}
-    />
-  ) : null;
+  const subTitle =
+    subtitle && type === TYPES.MESSAGE ? (
+      <DzText
+        textSize={TEXT_SIZES.XS}
+        textType={TEXT_TYPES.P}
+        text={subtitle}
+      />
+    ) : null;
 
   return (
-    <div className={cn(styles.bannerContainer, styles?.[variant])}>
+    <div
+      className={cn(
+        styles.bannerContainer,
+        styles?.[variant],
+        styles?.[type],
+        alertTypeStyles
+      )}
+    >
       <div className={cn(styles.textContainer)}>
         {titleSection}
         {subTitle}
--- stories/DzBanner.stories.tsx
+++ stories/DzBanner.stories.tsx
@@ -1,14 +1,26 @@
 import React from 'react';
 import { Meta, Story } from '@storybook/react';
-import { DzBanner, DzBannerProps, BANNER_VARIANT_NAMES } from '../src/atoms/DzBanner';
+import {
+  DzBanner,
+  DzBannerProps,
+  BANNER_VARIANT_NAMES,
+  BANNER_TYPES_NAMES,
+  ALERT_VARIANT_NAMES,
+} from '../src/atoms/DzBanner';
 
 const meta: Meta = {
   title: 'Atoms/Helpers/DzBanner',
   component: DzBanner,
   argTypes: {
     title: { type: 'string', defaultValue: 'Message Title' },
-    subtitle: { type: 'string', defaultValue: 'This is placeholder text. Ut sodales nunc vitae est lacinia, nec tempus risus aliquam. Vestibulum sollicitudin eget tellus ac venenatis. ' },
+    subtitle: {
+      type: 'string',
+      defaultValue:
+        'This is placeholder text. Ut sodales nunc vitae est lacinia, nec tempus risus aliquam. Vestibulum sollicitudin eget tellus ac venenatis. ',
+    },
     variant: { control: 'select', options: BANNER_VARIANT_NAMES },
+    type: { control: 'select', options: BANNER_TYPES_NAMES },
+    alertType: { control: 'select', options: ALERT_VARIANT_NAMES },
   },
   parameters: {
     controls: { expanded: true },
