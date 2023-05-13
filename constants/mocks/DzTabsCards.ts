ba99c18d7c09d8ac22d991c91b7f979323722ded
David Torres <jdtcto@gmail.com>
2023-05-12 19:43:26 -0500

    NWEB-323: add DzTabsCards component, useful for display locations

---
+++ constants/mocks/DzTabsCards.ts
@@ -0,0 +1,229 @@
+import { MEDIA_TYPES } from '../../src/atoms/DzMedia';
+
+export const tabs = [
+  {
+    title: 'New York',
+    cards: [
+      {
+        media: {
+          type: MEDIA_TYPES.IMAGE,
+          imgProps: {
+            src:
+              'https://assets.davidzwirner.com/v7/_assets_/davidzwirner/galleries/gallery-cover-images/webdzpanorama-by-jason-schmidt.jpg?w=1100&q=85&org_if_sml=1&force_format=webp',
+            alt: 'content',
+          },
+        },
+        category: 'Category Slug',
+        title: 'Lorem ipsum dolor sit amet, consectetuer adipiscin',
+        subtitle: 'Lorem ipsum dolor sit amet, consectetuer adipiscin',
+        secondaryTitle: 'Lorem ipsum dolor sit amet, consectetuer adipiscin',
+        secondarySubtitle: 'Lorem ipsum dolor sit amet, consectetuer adipiscin',
+        description:
+          'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec qu',
+        linkCTA: {
+          text: 'Learn More',
+          linkElement: 'a',
+          url: '/',
+        },
+        primaryCTA: {
+          text: 'Button',
+        },
+      },
+      {
+        media: {
+          type: MEDIA_TYPES.IMAGE,
+          imgProps: {
+            src:
+              'https://assets.davidzwirner.com/v7/_assets_/davidzwirner/galleries/gallery-cover-images/dz_gallery_20th_street_cover_image.jpg?w=1100&q=85&org_if_sml=1&force_format=webp',
+            alt: 'content',
+          },
+        },
+        category: 'Category Slug',
+        title: 'Lorem ipsum dolor sit amet, consectetuer adipiscin',
+        subtitle: 'Lorem ipsum dolor sit amet, consectetuer adipiscin',
+        secondaryTitle: 'Lorem ipsum dolor sit amet, consectetuer adipiscin',
+        secondarySubtitle: 'Lorem ipsum dolor sit amet, consectetuer adipiscin',
+        description:
+          'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec qu',
+        linkCTA: {
+          text: 'Learn More',
+          linkElement: 'a',
+          url: '/',
+        },
+        primaryCTA: {
+          text: 'Button',
+        },
+      },
+      {
+        media: {
+          type: MEDIA_TYPES.IMAGE,
+          imgProps: {
+            src:
+              'https://assets.davidzwirner.com/v7/_assets_/davidzwirner/galleries/gallery-cover-images/dz_gallery_69th_street_cover_image.jpg?w=1100&q=85&org_if_sml=1&force_format=webp',
+            alt: 'content',
+          },
+        },
+        category: 'Category Slug',
+        title: 'Lorem ipsum dolor sit amet, consectetuer adipiscin',
+        subtitle: 'Lorem ipsum dolor sit amet, consectetuer adipiscin',
+        secondaryTitle: 'Lorem ipsum dolor sit amet, consectetuer adipiscin',
+        secondarySubtitle: 'Lorem ipsum dolor sit amet, consectetuer adipiscin',
+        description:
+          'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec qu',
+        linkCTA: {
+          text: 'Learn More',
+          linkElement: 'a',
+          url: '/',
+        },
+        primaryCTA: {
+          text: 'Button',
+        },
+      },
+      {
+        media: {
+          type: MEDIA_TYPES.IMAGE,
+          imgProps: {
+            src:
+              'https://assets.davidzwirner.com/v7/_assets_/davidzwirner/galleries/gallery-cover-images/dz_gallery_52-walker.jpg?w=1100&q=85&org_if_sml=1&force_format=webp',
+            alt: 'content',
+          },
+        },
+        category: 'Category Slug',
+        title: 'Lorem ipsum dolor sit amet, consectetuer adipiscin',
+        subtitle: 'Lorem ipsum dolor sit amet, consectetuer adipiscin',
+        secondaryTitle: 'Lorem ipsum dolor sit amet, consectetuer adipiscin',
+        secondarySubtitle: 'Lorem ipsum dolor sit amet, consectetuer adipiscin',
+        description:
+          'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec qu',
+        linkCTA: {
+          text: 'Learn More',
+          linkElement: 'a',
+          url: '/',
+        },
+        primaryCTA: {
+          text: 'Button',
+        },
+      },
+    ],
+  },
+  {
+    title: 'Los Angeles',
+    cards: [
+      {
+        media: {
+          type: MEDIA_TYPES.IMAGE,
+          imgProps: {
+            src:
+              'https://assets.davidzwirner.com/v7/_assets_/davidzwirner/galleries/gallery-cover-images/la-rendering2.jpg?w=1100&q=85&org_if_sml=1&force_format=webp',
+            alt: 'content',
+          },
+        },
+        category: 'Category Slug',
+        title: 'Lorem ipsum dolor sit amet, consectetuer adipiscin',
+        subtitle: 'Lorem ipsum dolor sit amet, consectetuer adipiscin',
+        secondaryTitle: 'Lorem ipsum dolor sit amet, consectetuer adipiscin',
+        secondarySubtitle: 'Lorem ipsum dolor sit amet, consectetuer adipiscin',
+        description:
+          'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec qu',
+        linkCTA: {
+          text: 'Learn More',
+          linkElement: 'a',
+          url: '/',
+        },
+        primaryCTA: {
+          text: 'Button',
+        },
+      },
+    ],
+  },
+  {
+    title: 'London',
+    cards: [
+      {
+        media: {
+          type: MEDIA_TYPES.IMAGE,
+          imgProps: {
+            src:
+              'https://assets.davidzwirner.com/v7/_assets_/davidzwirner/galleries/gallery-cover-images/dz_gallery_london_cover_image.jpg?w=1100&q=85&org_if_sml=1&force_format=webp',
+            alt: 'content',
+          },
+        },
+        category: 'Category Slug',
+        title: 'Lorem ipsum dolor sit amet, consectetuer adipiscin',
+        subtitle: 'Lorem ipsum dolor sit amet, consectetuer adipiscin',
+        secondaryTitle: 'Lorem ipsum dolor sit amet, consectetuer adipiscin',
+        secondarySubtitle: 'Lorem ipsum dolor sit amet, consectetuer adipiscin',
+        description:
+          'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec qu',
+        linkCTA: {
+          text: 'Learn More',
+          linkElement: 'a',
+          url: '/',
+        },
+        primaryCTA: {
+          text: 'Button',
+        },
+      },
+    ],
+  },
+  {
+    title: 'Paris',
+    cards: [
+      {
+        media: {
+          type: MEDIA_TYPES.IMAGE,
+          imgProps: {
+            src:
+              'https://assets.davidzwirner.com/v7/_assets_/davidzwirner/galleries/gallery-cover-images/dz_gallery_paris_cover_image.jpg?w=1100&q=85&org_if_sml=1&force_format=webp',
+            alt: 'content',
+          },
+        },
+        category: 'Category Slug',
+        title: 'Lorem ipsum dolor sit amet, consectetuer adipiscin',
+        subtitle: 'Lorem ipsum dolor sit amet, consectetuer adipiscin',
+        secondaryTitle: 'Lorem ipsum dolor sit amet, consectetuer adipiscin',
+        secondarySubtitle: 'Lorem ipsum dolor sit amet, consectetuer adipiscin',
+        description:
+          'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec qu',
+        linkCTA: {
+          text: 'Learn More',
+          linkElement: 'a',
+          url: '/',
+        },
+        primaryCTA: {
+          text: 'Button',
+        },
+      },
+    ],
+  },
+  {
+    title: 'Hong Kong',
+    cards: [
+      {
+        media: {
+          type: MEDIA_TYPES.IMAGE,
+          imgProps: {
+            src:
+              'https://assets.davidzwirner.com/v7/_assets_/davidzwirner/galleries/gallery-cover-images/dz_gallery_hongkong_cover_image-min.jpg?w=1100&q=85&org_if_sml=1&force_format=webp',
+            alt: 'content',
+          },
+        },
+        category: 'Category Slug',
+        title: 'Lorem ipsum dolor sit amet, consectetuer adipiscin',
+        subtitle: 'Lorem ipsum dolor sit amet, consectetuer adipiscin',
+        secondaryTitle: 'Lorem ipsum dolor sit amet, consectetuer adipiscin',
+        secondarySubtitle: 'Lorem ipsum dolor sit amet, consectetuer adipiscin',
+        description:
+          'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec qu',
+        linkCTA: {
+          text: 'Learn More',
+          linkElement: 'a',
+          url: '/',
+        },
+        primaryCTA: {
+          text: 'Button',
+        },
+      },
+    ],
+  },
+];
