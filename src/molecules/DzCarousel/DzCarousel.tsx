a98d7eb28032aa8b4c99b2e96718ee0d328730be
Fabio Tovar <fabiostovarr@gmail.com>
2023-06-16 15:24:27 -0500

    Add snap on release prop to carousel

--- src/molecules/DzCarousel/DzCarousel.tsx
+++ src/molecules/DzCarousel/DzCarousel.tsx
@@ -5,7 +5,6 @@ import React, {
   useState,
   useEffect,
   Fragment,
-  useLayoutEffect,
 } from 'react';
 import { BREAKPOINTS } from '../../layout/breakpoints';
 import useWindowSize from '../../hooks/useWindowSize';
@@ -110,6 +109,7 @@ export const DzCarousel: React.FunctionComponent<DzCarouselProps> = ({
         mousewheel="true"
         mousewheel-force-to-axis="true"
         scrollbar-draggable="true"
+        scrollbar-snap-on-release="false"
         slides-per-view="auto"
         slides-offset-before={OFFSET_BEFORE}
         slides-offset-after={OFFSET_AFTER}
