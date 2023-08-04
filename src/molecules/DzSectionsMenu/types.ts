b97b8053876098378bb121f632d76b8044502815
David Torres <jdtcto@gmail.com>
2023-08-04 14:57:21 -0500

    NWEB-488: add extra props for dzmedia and sections menu

--- src/molecules/DzSectionsMenu/types.ts
+++ src/molecules/DzSectionsMenu/types.ts
@@ -1,12 +1,14 @@
-import { DzButtonProps } from '../../atoms/DzButton';
+import { DzButtonProps, DzLinkProps } from '../../atoms';
 
 export interface DzSectionMenuProps {
   sections?: SectionNavItem[];
-  cta: SectionMenuCTA;
+  cta?: SectionMenuCTA;
   onSelection?: Function;
   prefix?: string;
   usePrefix?: boolean;
   sticky?: boolean;
+  useLinks?: boolean;
+  linksProps?: DzLinkProps;
 }
 
 interface SectionMenuCTA {
@@ -17,4 +19,5 @@ interface SectionMenuCTA {
 export interface SectionNavItem {
   text: string;
   id: string;
+  url?: string;
 }
