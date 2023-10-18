cb1950062bd8c110abd1f557835037cd5a5e5d1b
adam krawesky <adam.krawesky@gmail.com>
2023-10-18 12:43:15 -0400

    fix char limit truncation

--- src/molecules/DzPortableText/limitCharacterCount.ts
+++ src/molecules/DzPortableText/limitCharacterCount.ts
@@ -6,16 +6,16 @@ export const limitCharacterCount = (ptItems: any, charLimit: number) => {
   return ptItems.map((item: any) => {
     const children = item.children.map((child: any) => {
       const { text } = child;
-
       charCount = charCount + (text || '').length;
+      const charsOverLimit = charCount - charLimit;
 
-      if (charCount > charLimit) {
+      if (charsOverLimit > 0) {
         if (!isOverLimit) {
           isOverLimit = true;
 
           return {
             ...child,
-            text: (text || '').slice(0, -4).concat('...'),
+            text: (text || '').slice(0, -charsOverLimit).concat('...'),
           };
         }
         return {
