e9378380a99208290bda1d93c13ec9a4994750c2
PsychicPower <psycpv@gmail.com>
2023-11-17 22:38:36 +0100

    fix: fix linter

--- stories/molecules/DzPromoModal.stories.tsx
+++ stories/molecules/DzPromoModal.stories.tsx
@@ -21,10 +21,12 @@ export default meta;
 const Template: Story<DzPromoModalProps> = args => <DzPromoModal {...args} />;
 
 export const DzPromoModalSample = Template.bind({});
-DzPromoModalSample.args = { 
-  title:'Utopia Editions: Lucas Arruda',
-  subtitle:'A new etching by Lucas Arruda echoes the artist’s meditative approach to painting.',
-  buttonText:'Explore',
-  imgSrc:'https://cdn.sanity.io/images/juzvn5an/test/eb12cba8eedbe3d418d0f6ad8db889d55862006d-1080x1920.jpg?w=2000&fit=max&auto=format',
-  imgAlt:'leaf',
- };
+DzPromoModalSample.args = {
+  title: 'Utopia Editions: Lucas Arruda',
+  subtitle:
+    'A new etching by Lucas Arruda echoes the artist’s meditative approach to painting.',
+  buttonText: 'Explore',
+  imgSrc:
+    'https://cdn.sanity.io/images/juzvn5an/test/eb12cba8eedbe3d418d0f6ad8db889d55862006d-1080x1920.jpg?w=2000&fit=max&auto=format',
+  imgAlt: 'leaf',
+};
