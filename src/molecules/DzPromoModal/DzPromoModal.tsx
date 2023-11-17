b6651143a37b1063f87c749849c46b91c3fd1223
PsychicPower <psycpv@gmail.com>
2023-11-17 22:33:57 +0100

    feat: add DzPromoModal modal

---
+++ src/molecules/DzPromoModal/DzPromoModal.tsx
@@ -0,0 +1,80 @@
+import React, { useEffect } from 'react';
+import {
+  DzButton,
+  DzModalContainer,
+  DzTitle,
+  TITLE_SIZES,
+  TITLE_TYPES,
+} from '../../atoms';
+import useLockedBodyScroll from '../../hooks/useLockedBodyScroll';
+
+export type DzPromoModalProps = {
+  isOpen: boolean;
+  onClose: () => void;
+  title: string;
+  subtitle?: string;
+  buttonText: string;
+  onClick: () => void;
+  imgSrc: string;
+  imgAlt: string;
+};
+
+export const DzPromoModal = ({
+  isOpen,
+  onClose,
+  title,
+  subtitle,
+  buttonText,
+  onClick,
+  imgSrc,
+  imgAlt,
+}: DzPromoModalProps) => {
+  const [, setIsBodyScrollLocked] = useLockedBodyScroll(false, 'root');
+  const onCloseModal = () => {
+    onClose();
+  };
+  useEffect(() => setIsBodyScrollLocked(isOpen), [
+    isOpen,
+    setIsBodyScrollLocked,
+  ]);
+
+  return (
+    <DzModalContainer
+      isOpen={isOpen}
+      onClose={onCloseModal}
+      className="max-w-[57.875rem] md:max-h-[37.5rem] p-0 flex flex-col md:flex-row"
+    >
+      <div className="md:w-1/2 md:max-h-[37.5rem] overflow-hidden">
+        <img
+          src={imgSrc}
+          className="w-full h-[240px] md:h-full object-cover"
+          alt={imgAlt}
+        />
+      </div>
+      <div className="p-5 flex flex-col md:justify-between w-full md:w-1/2">
+        <div>
+          <DzTitle
+            titleType={TITLE_TYPES.H1}
+            titleSize={TITLE_SIZES.XL}
+            title={title}
+          />
+          <DzTitle
+            className="pt-1"
+            titleType={TITLE_TYPES.P}
+            titleSize={TITLE_SIZES.MD}
+            title={subtitle}
+          />
+        </div>
+        <DzButton
+          onClick={() => onClick()}
+          size="large"
+          className="w-full mt-10"
+        >
+          {buttonText}
+        </DzButton>
+      </div>
+    </DzModalContainer>
+  );
+};
+
+export default DzPromoModal;
---
+++ stories/molecules/DzPromoModal.stories.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Meta, Story } from '@storybook/react';
+import {
+  DzPromoModal,
+  DzPromoModalProps,
+} from '../../src/molecules/DzPromoModal/DzPromoModal';
+
+const meta: Meta = {
+  title: 'Molecules/Content/DzPromoModal',
+  component: DzPromoModal,
+  parameters: {
+    controls: { expanded: true },
+    backgrounds: {
+      default: 'dzFigmaWhite',
+    },
+  },
+};
+
+export default meta;
+
+const Template: Story<DzPromoModalProps> = args => <DzPromoModal {...args} />;
+
+export const DzPromoModalSample = Template.bind({});
+DzPromoModalSample.args = { 
+  title:'Utopia Editions: Lucas Arruda',
+  subtitle:'A new etching by Lucas Arruda echoes the artist’s meditative approach to painting.',
+  buttonText:'Explore',
+  imgSrc:'https://cdn.sanity.io/images/juzvn5an/test/eb12cba8eedbe3d418d0f6ad8db889d55862006d-1080x1920.jpg?w=2000&fit=max&auto=format',
+  imgAlt:'leaf',
+ };
